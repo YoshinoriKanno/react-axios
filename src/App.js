552afe2f42ee8451e1636dfbc950fd51dae8ac7c
Yoshinori Kanno <yoshinori.kanno@me.com>
2023-03-20 17:54:13 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+// import AxiosGet from "./axios-get";
+// import AxiosPost from "./axios-post";
+// import AxiosPut from "./axios-put";
+// import AxiosDelete from "./axios-delete";
+// import AxiosError from "./axios-error";
+// import AxiosClient from "./axios-client";
+// import AxiosAsyncAwait from "./axios-async-await";
+import AxiosUseAxios from "./axios-use-axios";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <hr />
+      {/* <AxiosGet /> */}
+      {/* <AxiosPost /> */}
+      {/* <AxiosPut /> */}
+      {/* <AxiosError /> */}
+      {/* <AxiosClient /> */}
+      {/* <AxiosAsyncAwait /> */}
+      <AxiosUseAxios />
+    </div>
+  );
+}
